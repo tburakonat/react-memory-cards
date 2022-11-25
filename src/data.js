d552a11cc30bc3bc10b6b8d8b2741fbe77285f81
tburakonat <tburakonat@gmail.com>
2022-11-25 23:29:41 +0100

    add initial data

---
+++ src/data.js
@@ -0,0 +1,57 @@
+import anakin from './images/anakin.jpeg';
+import obiwan from './images/obiwan.jpeg';
+import luke from './images/luke.jpeg';
+import vader from './images/vader.jpeg';
+import c3po from './images/c3po.jpeg';
+import r2d2 from './images/r2d2.jpeg';
+import chewie from './images/chewie.jpeg';
+import maul from './images/maul.jpeg';
+import yoda from './images/yoda.jpeg';
+
+export const data = [
+	{
+		id: 1,
+		src: anakin,
+		title: 'Anakin',
+	},
+	{
+		id: 2,
+		src: obiwan,
+		title: 'Obiwan',
+	},
+	{
+		id: 3,
+		src: luke,
+		title: 'Luke',
+	},
+	{
+		id: 4,
+		src: vader,
+		title: 'Vader',
+	},
+	{
+		id: 5,
+		src: c3po,
+		title: 'C3PO',
+	},
+	{
+		id: 6,
+		src: r2d2,
+		title: 'R2D2',
+	},
+	{
+		id: 7,
+		src: chewie,
+		title: 'Chewie',
+	},
+	{
+		id: 8,
+		src: maul,
+		title: 'Maul',
+	},
+	{
+		id: 9,
+		src: yoda,
+		title: 'Yoda',
+	},
+];
