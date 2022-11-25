6b7cb40ddc9933bc49c38050f1b4a92ad53e3a8a
tburakonat <tburakonat@gmail.com>
2022-11-25 23:32:35 +0100

    pass state to memory board

--- src/App.js
+++ src/App.js
@@ -7,10 +7,32 @@ function App() {
 	const [currentScore, setCurrentScore] = useState(0);
 	const [highScore, setHighScore] = useState(0);
 
+	const playRound = cardId => {
+		if (clickedItems.includes(cardId)) {
+			resetGame();
+		} else {
+			const newScore = currentScore + 1;
+			if (newScore > highScore) setHighScore(newScore);
+			setCurrentScore(newScore);
+			setClickedItems(prevState => [...prevState, cardId]);
+		}
+	};
+
+	const resetGame = () => {
+		setClickedItems([]);
+		setCurrentScore(0);
+	};
+
+	useEffect(() => {
+		if (currentScore > highScore) {
+			setHighScore(currentScore);
+		}
+	}, [currentScore, highScore]);
+
 	return (
 		<div className="d-flex flex-column" style={{ backgroundColor: 'black', minHeight: '100vh' }}>
 			<Header currentScore={currentScore} highScore={highScore} />
-			<MemoryBoard />
+			<MemoryBoard handleClick={playRound} />
 		</div>
 	);
 }
--- src/components/MemoryBoard.js
+++ src/components/MemoryBoard.js
@@ -1,77 +1,34 @@
-import React from 'react';
-import anakin from './../images/anakin.jpeg';
-import obiwan from './../images/obiwan.jpeg';
-import luke from './../images/luke.jpeg';
-import vader from './../images/vader.jpeg';
-import c3po from './../images/c3po.jpeg';
-import r2d2 from './../images/r2d2.jpeg';
-import chewie from './../images/chewie.jpeg';
-import maul from './../images/maul.jpeg';
-import yoda from './../images/yoda.jpeg';
+import React, { useEffect, useState } from 'react';
+import { data } from '../data';
 
-const MemoryBoard = () => {
+const MemoryBoard = ({ handleClick }) => {
 	const styles = {
-		backgroundColor: 'green',
-		display: 'grid',
-		gridTemplateColumns: 'repeat(3, 1fr)',
-		gridAutoRows: 'max-content',
+		display: 'flex',
+		flexWrap: 'wrap',
+		gap: '30px',
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: '0 50px 50px',
 	};
 
-	const cards = [
-		{
-			id: 1,
-			src: anakin,
-			title: 'Anakin',
-		},
-		{
-			id: 2,
-			src: obiwan,
-			title: 'Obiwan',
-		},
-		{
-			id: 3,
-			src: luke,
-			title: 'Luke',
-		},
-		{
-			id: 4,
-			src: vader,
-			title: 'Vader',
-		},
-		{
-			id: 5,
-			src: c3po,
-			title: 'C3PO',
-		},
-		{
-			id: 6,
-			src: r2d2,
-			title: 'R2D2',
-		},
-		{
-			id: 7,
-			src: chewie,
-			title: 'Chewie',
-		},
-		{
-			id: 8,
-			src: maul,
-			title: 'Maul',
-		},
-		{
-			id: 9,
-			src: yoda,
-			title: 'Yoda',
-		},
-	];
+	const [cards, setCards] = useState(data);
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	useEffect(() => {
+		setCards(prevCards => prevCards.sort(() => Math.random() - 0.5));
+	});
 
 	return (
 		<div style={styles}>
 			{cards.map(card => {
 				return (
-					<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-						{card.title}
-						<img key={card.id} src={card.src} alt={`${card.title} logo`} width="160px" />
+					<div
+						key={card.id}
+						style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+						onClick={() => handleClick(card.id)}
+					>
+						<span className="text-white">{card.title}</span>
+						<img src={card.src} alt={`${card.title} logo`} width="200px" />
 					</div>
 				);
 			})}
